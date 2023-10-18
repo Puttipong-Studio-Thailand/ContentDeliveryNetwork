821ff5c8b55c3f3a1d15734bba91a67d3aa787e2
Poohkungz <128376705+Poohkungz@users.noreply.github.com>
2023-10-18 17:47:00 +0700

    UPDATE PHP

---
+++ connect.php
@@ -0,0 +1,17 @@
+
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=u295569765_cdnhosting;charset=utf8", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  //echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
+//Set ว/ด/ป เวลา ให้เป็นของประเทศไทย
+    date_default_timezone_set('Asia/Bangkok');
+?>
